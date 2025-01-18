029e9a37b8fd3e3f08d0d10a777c9bc3b198c232
Anesu Ngirande <anesupaulngirande@gmail.com>
2025-01-18 18:44:10 +0200

    format response

--- frontend/src/app/Http/Resources/ArticleResource.php
+++ frontend/src/app/Http/Resources/ArticleResource.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $category
+ * @property mixed $author
+ * @property mixed $id
+ * @property mixed $source
+ * @property mixed $title
+ * @property mixed $description
+ * @property mixed $url
+ * @property mixed $image_url
+ * @property mixed $published_at
+ *
+ */
 class ArticleResource extends JsonResource
 {
     /**
@@ -14,6 +27,16 @@ class ArticleResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id" => $this->id,
+            "source" => $this->source,
+            "author" => $this->author,
+            "category" => $this->category,
+            "title" => $this->title,
+            "description" => $this->description,
+            "url" => $this->url,
+            "image_url" => $this->image_url,
+            "published_at" => Carbon::createFromTimestamp($this->published_at)->format('Y-m-d')
+        ];
     }
 }
